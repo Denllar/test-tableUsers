77d80032f28a3e64445dc650e1c28c8946450dd8
Denllar <denisgabbasov06.09.2004@gmail.com>
2024-02-05 19:22:42 +0500

    fix Route path

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/test-tableUsers' element={<Home />}></Route>
       </Routes>
     </>
   )
